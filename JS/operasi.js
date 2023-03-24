82bb636cb3d45fc24b14d87fc36ae9ddecd9e138
HADAIZI <adamhadaizi2002@gmail.com>
2023-03-24 14:57:36 +0700

    Tadi salah nama

--- JS/operasi.js
+++
@@ -1,7 +0,0 @@
-const { bagi, tambah, kurang } = require('./aritmatika');
-
-const hasil = bagi(4000, 2);
-const hasil2 = tambah(hasil, 25);
-const hasil3 = kurang(hasil2, 2);
-
-console.log(hasil3);
