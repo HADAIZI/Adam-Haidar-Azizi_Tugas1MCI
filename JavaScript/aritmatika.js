449a0ff10da6a5c5801f3f70908520da562ad5bb
HADAIZI <adamhadaizi2002@gmail.com>
2023-03-24 14:55:39 +0700

    tadi salah nama

---
+++ JavaScript/aritmatika.js
@@ -0,0 +1,6 @@
+const tambah = (a, b) => a + b;
+const kurang = (a, b) => a - b;
+const kali = (a, b) => a * b;
+const bagi = (a, b) => a / b;
+
+module.exports = { tambah, kurang, kali, bagi };
---
+++ JavaScript/no_1.js
@@ -0,0 +1,13 @@
+const displayMessage = function(callback) {
+    console.log("Saya Calon Admin MCI");
+    setTimeout(callback, 3000);
+  }
+  
+  const showName = function() {
+    console.log("Adam Haidar Azizi");
+  }
+  
+  displayMessage(showName);
+
+  
+  
